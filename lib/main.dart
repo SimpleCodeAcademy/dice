642e87204a8bb3a71f5b61437012ca0e7eb903c6
khongorzulodonkhuu <khongorzul.o@zerotech.mn>
2025-07-05 09:36:44 +0800

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,82 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blue,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.blue,
-        ),
-        body: DicePage(),
-      ),
-    ),
-  );
+  runApp(const DiceApp());
 }
 
-class DicePage extends StatelessWidget {
-  const DicePage({super.key});
+class DiceApp extends StatelessWidget {
+  const DiceApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Dice App',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const DiceHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class DiceHomePage extends StatefulWidget {
+  const DiceHomePage({super.key});
+
+  @override
+  State<DiceHomePage> createState() => _DiceHomePageState();
+}
+
+class _DiceHomePageState extends State<DiceHomePage> {
+  int leftDice = 1;
+  int rightDice = 1;
+
+  void rollDice() {
+    setState(() {
+      leftDice = Random().nextInt(6) + 1;
+      rightDice = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text(
+          'Dice App',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'images/dice$leftDice.png',
+                  width: 100,
+                ),
+                const SizedBox(width: 16),
+                Image.asset(
+                  'images/dice$rightDice.png',
+                  width: 100,
+                ),
+              ],
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: rollDice,
+              child: const Text('Roll Dice'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
